912ff49ba516809d56289a4b9808d093ecf68959
yoohyunk <yoohyunk20@gmail.com>
2025-05-17 21:53:15 -0600

    Update Project type definition with new properties

--- src/types/project.ts
+++ src/types/project.ts
@@ -4,4 +4,11 @@ export interface Project {
   description: string;
   tech: string[];
   link: string;
+
+  // New fields used in components
+  image: string;
+  technologies: string[];
+  features?: string[];
+  liveUrl?: string;
+  repoUrl?: string;
 }
