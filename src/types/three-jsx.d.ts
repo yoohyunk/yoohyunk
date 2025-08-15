e64e52786d52ecb4540834473c7a329ff144f3a3
yoohyunk <yoohyunk20@gmail.com>
2025-08-14 19:34:10 -0600

    resolve TypeScript build errors for Three.js components

--- src/types/three-jsx.d.ts
+++ src/types/three-jsx.d.ts
@@ -1,7 +1,14 @@
-import { ThreeElements } from "@react-three/fiber";
+/// <reference types="@react-three/fiber" />
 
 declare global {
   namespace JSX {
-    interface IntrinsicElements extends ThreeElements {}
+    interface IntrinsicElements {
+      ambientLight: any;
+      directionalLight: any;
+      group: any;
+      primitive: any;
+      color: any;
+      pointLight: any;
+    }
   }
 }
